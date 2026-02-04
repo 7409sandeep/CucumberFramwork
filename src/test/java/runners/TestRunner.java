e00fd2772702a787827f6e059b0d1b2bc4034ad8
SANDEEP SINGH <7409sandeep1@gmail.com>
2026-02-04 14:58:19 +0530

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,9 +1,9 @@
 package runners;
- 
+
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
- 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/Features",
@@ -11,7 +11,8 @@ import io.cucumber.junit.CucumberOptions;
         monochrome = true,
         plugin = {
                 "pretty",
-                "html:target/cucumber-report.html"
+                "json:target/cucumber-reports/cucumber.json",
+                "html:target/cucumber-reports/cucumber.html"
         }
 )
 public class TestRunner {
